05ef8dc23da43cbf82d93024677d8685db12d395
franciscomarin98 <francmarinc@gmail.com>
2021-11-17 16:48:15 -0500

    :white_check_mark:  test: Component Testing - GifItem

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 const GifItem = ({url, title}) => {
 
     return (
-        <div className="col">
-            <div className="card h-100 animate__animated animate__fadeIn">
-                <img src={url} alt="imagen"/>
+        <div className="col animate__animated animate__fadeIn">
+            <div className="card h-100">
+                <img src={url} alt={title}/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                 </div>
--- src/tests/components/GifItem.test.js
+++ src/tests/components/GifItem.test.js
@@ -2,12 +2,31 @@ import {shallow} from "enzyme";
 import GifItem from "../../components/GifItem";
 
 describe('Tests in <GifItem/>', () => {
-    test('should show the component successfully', () => {
 
-        const url = 'https://example.com';
-        const title = 'example';
+    const url = 'https://example.com';
+    const title = 'example';
+    const wrapper = shallow(<GifItem title={title} url={url}/>);
 
-        const wrapper = shallow(<GifItem title={title} url={url}/>);
+    test('It should show the component successfully', () => {
         expect(wrapper).toMatchSnapshot();
-    })
+    });
+
+    test('It should have a header element of type h5 and inside the title that is sent by props.', () => {
+        const element = wrapper.find('h5');
+        expect(element.text().trim()).toBe(title);
+    });
+
+    test('It should show the image with the information sent by props', () => {
+        const elementImg = wrapper.find('img');
+        // console.log(elementImg.html());
+        // console.log(elementImg.props());
+        expect(elementImg.prop('src')).toBe(url);
+        expect(elementImg.prop('alt')).toBe(title);
+    });
+
+    test('The card container should have the class: animate__fadeIn', () => {
+        // expect(wrapper.prop('className').includes('animate__fadeIn')).toBe(true);
+        expect(wrapper.hasClass('animate__fadeIn')).toBeTruthy();
+    });
+
 })
